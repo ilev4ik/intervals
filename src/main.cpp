be1de0a6a4a222460c395935c8299afccceb3b38
Levon <ilev4ik@gmail.com>
2019-04-18 20:46:37 +0300

    example updated with tagged_data

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,41 @@
 using namespace lvn;
 using namespace lvn::detail;
 
+template <typename D>
+struct tagged_data {
+    int id;
+    D data;
+
+    // for multiset
+    friend bool operator< (const tagged_data<D>& lhs, const tagged_data<D>& rhs) {
+        return lhs.data < rhs.data;
+    }
+
+    // for adjacent_find and groupings
+    friend bool operator== (const tagged_data<D>& lhs, const tagged_data<D>& rhs) {
+        return lhs.id == rhs.id && lhs.data == rhs.data;
+    }
+
+    // for debugging :)
+    friend std::ostream& operator<< (std::ostream& os, const tagged_data<D>& d) {
+        return os << "{" << d.id << ", " << d.data << "}";
+    }
+};
+
 int main()
 {
-    using stamps_t = stamps_set_t<int, std::string>;
+    using stamper_type = stamps_accumulator<int, tagged_data<std::string>>;
+    using stamps_t = typename stamper_type::stamps_set_t;
+
     stamps_t stamps1 = {
-            {0, true, "11::"},
-            {10, true, "22::"},
-            {20, false, "11::"}
+            {0, true, {1, "11::"}},
+            {10, true, {2, "22::"}},
+            {10, true, {4, "44::"}},
+            {20, false, {1, "11::"}}
     };
 
-    stamps_accumulator<int, std::string> stamper;
+
+    stamper_type stamper;
     for (auto&& i : stamper.add(stamps1)) {
         lvn::print_iterable_range(i);
     }
@@ -26,20 +51,28 @@ int main()
     lvn::underline();
 
     stamps_t stamps2 = {
-            {0, true, "22::"},
-            {10, true, "44::"},
-            {20, false, "44::"},
-            {50, true, "11::"},
-            {100, false, "22::"}
+            {0, true, {2, "22::"}},
+            {10, true, {4, "44::"}},
+            {20, false, {4, "44::"}},
+            {50, true, {1, "11::"}},
+            {100, false, {2, "22::"}}
     };
+
+
     for (auto&& i : stamper.add(stamps2)) {
         lvn::print_iterable_range(i);
     }
     lvn::underline();
 
     stamper.intersect();
+
     for (auto&& segment : stamper.result()) {
-        std::cout << segment << std::endl;
+        std::cout << segment << " => \n";
+        std::cout << "\t collisions: ";
+        for (auto&& collision : segment.collisions()) {
+            std::cout << collision << " ";
+        }
+        std::cout << "\n\n";
     }
 
     return 0;
