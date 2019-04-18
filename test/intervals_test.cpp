d71ab27bad15be64819c8a6144cf202783c956a6
Levon <ilev4ik@gmail.com>
2019-04-18 20:55:17 +0300

    tests updated

--- test/intervals_test.cpp
+++ test/intervals_test.cpp
@@ -15,8 +15,11 @@ namespace {
     template <typename T = int, typename D = std::string>
     using default_empty_segments = empty_segment_vector<T, D>;
 
+    template <typename T = int, typename D = std::string>
+    using default_stamps_set = std::multiset<default_stamp<T, D>>;
+
     template <typename T, typename D>
-    auto active_segments_range(const std::set<default_stamp<T, D>>& stamps)
+    std::vector<lvn::segment<T, D>> active_segments_range(const default_stamps_set<T, D>& stamps)
     {
         return lvn::active_segments(stamps.begin(), stamps.end());
     }
@@ -24,43 +27,43 @@ namespace {
 
 TEST(TestActiveSegments, Empty)
 {
-    std::set<default_stamp<>> stamps;
+    default_stamps_set<> stamps;
     ASSERT_EQ(active_segments_range(stamps), default_empty_segments<>{});
 }
 
 TEST(TestActiveSegments, SingleActiveStamp)
 {
-    std::set<default_stamp<>> stamps = {{0, true}};
+    default_stamps_set<> stamps = {{0, true}};
     ASSERT_TRUE((active_segments_range(stamps) == default_empty_segments<>{{0, boost::none}}));
 }
 
 TEST(TestActiveSegments, AllActiveStamps)
 {
-    std::set<default_stamp<>> stamps = {{0, true}, {10, true}, {20, true}};
+    default_stamps_set<> stamps = {{0, true}, {10, true}, {20, true}};
     ASSERT_TRUE((active_segments_range(stamps) == default_empty_segments<>{{0, boost::none}}));
 }
 
 TEST(TestInactiveSegments, SingleInactiveStamp)
 {
-    std::set<default_stamp<>> stamps = {{0, false}};
+    default_stamps_set<> stamps = {{0, false}};
     ASSERT_EQ(active_segments_range(stamps), default_empty_segments<>{});
 }
 
 TEST(TestInactiveSegments, AllInactiveStamps)
 {
-    std::set<default_stamp<>> stamps = {{0, false}, {10, false}, {20, false}};
+    default_stamps_set<> stamps = {{0, false}, {10, false}, {20, false}};
     ASSERT_EQ(active_segments_range(stamps), default_empty_segments<>{});
 }
 
 TEST(TestActiveSegments, MultipleActiveStampWithOffset)
 {
-    std::set<default_stamp<>> stamps = {{0, false}, {5, true}, {10, true}};
+    default_stamps_set<> stamps = {{0, false}, {5, true}, {10, true}};
     ASSERT_TRUE((active_segments_range(stamps) == default_empty_segments<>{{5, boost::none}}));
 }
 
 TEST(TestComplex, OffsetSingleActive)
 {
-    std::set<default_stamp<>> stamps = {
+    default_stamps_set<> stamps = {
             {0, false}, {10, false}, {20, true}, {25, false}
     };
 
@@ -69,7 +72,7 @@ TEST(TestComplex, OffsetSingleActive)
 
 TEST(TestComplex, OffsetSingleInactive)
 {
-    std::set<default_stamp<>> stamps = {
+    default_stamps_set<> stamps = {
             {0, true}, {10, true}, {20, false}, {25, true}
     };
 
@@ -81,7 +84,7 @@ TEST(TestComplex, OffsetSingleInactive)
 
 TEST(TestComplex, Alternating)
 {
-    std::set<default_stamp<>> stamps = {
+    default_stamps_set<> stamps = {
             {0, true}, {10, false}, {20, true},
             {50, false}, {100, true}, {200, false},
             {250, true}, {300, false}, {310, true},
@@ -99,7 +102,7 @@ TEST(TestComplex, Alternating)
 
 TEST(TestComplex, AlternatingWithOffset)
 {
-    std::set<default_stamp<>> stamps = {
+    default_stamps_set<> stamps = {
             {0, false}, {10, true}, {20, false},
             {50, true}, {100, false}, {200, true},
             {250, false}, {300, true}, {310, false},
@@ -117,7 +120,7 @@ TEST(TestComplex, AlternatingWithOffset)
 
 TEST(TestComplex, MixActiveWithInactive)
 {
-    std::set<default_stamp<>> stamps = {
+    default_stamps_set<> stamps = {
             {0, false}, {10, false}, {20, true},
             {50, true}, {100, true}, {200, false},
             {250, true}, {300, true}, {310, false},
